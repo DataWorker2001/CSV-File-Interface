5d3810336f3595074a3f45a8c03d733fcabea19d
ARPAN CHOUDHURY <arpanchoudhury.mit@gmail.com>
2023-06-28 23:46:37 +0530

    CSV Files Viewer Advanced.

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const multer = require('multer');
+const csv = require('csv-parser');
+const fs = require('fs');
+const ejs = require('ejs');
+
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Routes
+app.get('/', (req, res) => {
+  const files = fs.readdirSync('uploads/');
+  res.render('index', { files });
+});
+
+app.post('/upload', upload.single('csvFile'), (req, res) => {
+  res.redirect('/');
+});
+
+app.get('/files/:filename', (req, res) => {
+  const { filename } = req.params;
+  const rows = [];
+
+  fs.createReadStream(`uploads/${filename}`)
+    .pipe(csv())
+    .on('data', (row) => rows.push(row))
+    .on('end', () => {
+      res.render('csv', { rows });
+    });
+});
+
+app.get('/delete/:filename', (req, res) => {
+  const { filename } = req.params;
+
+  fs.unlink(`uploads/${filename}`, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error deleting file');
+    }
+
+    res.redirect('/');
+  });
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
